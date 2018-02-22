f1f1701815e2e7d0f519e5ad54664e0ff4e6c057
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-22 22:44:53 +0100

    extract-webpack... css-loader sass-loader
    fix me!

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'main.css'
+});
 
 module.exports = {
   entry: './lib/index.js',
@@ -15,6 +20,12 @@ module.exports = {
         query: {
           presets: ['env']
         }
+      },
+      {
+        test: /\.sass$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
       }
     ]
   },
