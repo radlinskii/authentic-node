e51463900bd6040b65d46f20a70291d710bc60c2
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-23 23:14:16 +0100

    disabled eslint no-unused-vars rule

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import styles from './style.css';
+import styles from './style.css'; //eslint-disable-line no-unused-vars
 
 ReactDOM.render(
   <App />,
