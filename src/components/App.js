b47b0f1d74a0223103f217977536da8b27dc1bbd
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-24 00:47:36 +0100

    added react-router and basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route as Route,
+  Link as Link,
+} from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      answer: 42,
-    };
-    this.asyncFunction = this.asyncFunction.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-  }
-
-  asyncFunction () {
-    return Promise.resolve(37);
-  }
-
-  async componentDidMount () {
-    this.setState({
-      answer: await this.asyncFunction(),
-    });
   }
 
   render() {
     return (
-      <div className='container app-component'>
-        <h1>Opa Opa</h1>
-        <div className="btn btn-info">
-          <span className="fa fa-star"></span>
-          {this.state.answer}
+      <Router>
+        <div className='container app-component'>
+          <nav className='navbar navbar-inverse'>
+            <div>
+              <div className='navbar-header'>
+                <Link className='navbar-brand' to='/'>Spider-Flower</Link>
+              </div>
+              <ul className='nav navbar-nav'>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+              </ul>
+              <ul className='nav navbar-nav navbar-right'>
+                <li><Link to='/login'><span className='fa fa-sign-in'>&nbsp;</span>Login</Link></li>
+                <li><Link to='/register'><span className='fa fa-user-plus'>&nbsp;</span>Register</Link></li>
+              </ul>
+            </div>
+          </nav>
+
+
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/about' component={About}/>
         </div>
-      </div>
+      </Router>
     );
   }
 }
