74b9313a66a32470286c2c9ba35537367cbd5644
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-23 08:59:55 +0100

    added sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const extractPlugin = new ExtractTextPlugin({
-  filename: 'main.css'
-});
 
 module.exports = {
   entry: './lib/index.js',
@@ -20,12 +15,6 @@ module.exports = {
         query: {
           presets: ['env']
         }
-      },
-      {
-        test: /\.sass$/,
-        use: extractPlugin.extract({
-          use: ['css-loader', 'sass-loader']
-        })
       }
     ]
   },
