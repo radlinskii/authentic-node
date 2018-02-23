59e78b369aae89fe1a37eea5d11b25a16b60b548
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-23 18:14:22 +0100

    added css-loader and style-loader to webpack to bundle all the style inline in bundle.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='container app-component'>
+        <h1>Opa Opa</h1>
         <div className="btn btn-info">
           <span className="fa fa-star"></span>
         </div>
