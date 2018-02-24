aa7bfd857cb4c18a1b3469abccd4a502219e5388
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-24 22:44:19 +0100

    fixed hardcoded link in api

--- src/api/toDoApi.js
+++ src/api/toDoApi.js
@@ -1,18 +1,7 @@
 class toDoApi {
   static getAllToDos() {
-    return fetch('http://localhost:8080/todo/list').then(response => {
+    return fetch('/todo/list').then(response => {
       return response.json();
-      /*[{
-        id: 1,
-        name: 'aga',
-        age: 21,
-      },
-      {
-        id: 2,
-        name: 'igi',
-        age: 22,
-      },
-      ];*/
     }).catch(error => {
       return error;
     });
