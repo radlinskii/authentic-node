59d0af6523d5ede0a2e5f7eb65e2ccb8776699f4
radlinskii <radlinsk@student.agh.edu.pl>
2018-02-26 17:08:05 +0100

    :zap: inserting one, selecting all and one todos

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,34 @@
+/* eslint-disable no-console */
 import express from 'express';
+import {MongoClient as mongodb,} from 'mongodb';
 
 const adminRouter = express.Router();
 
+let todos = [
+  {
+    title: 'learn how to manage time',
+    author: 'ignac',
+  },
+  {
+    title: 'CRACKING THE CODING INTERVIEW',
+    author: 'aga',
+  },
+];
+let todo = todos[0];
 adminRouter.route('/addTodos')
   .get((req, res) => {
-    res.send('inserting books');
-  });
+    const url = 'mongodb://127.0.0.1:27017/';
 
+    mongodb.connect(url, (err, db) => {
+      if(err) console.log(err);
+      const dbo = db.db('todoapp');
+      dbo.collection('todos').insertOne(todo, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send(results);
+        db.close();
+      });
+    });
+  });
 
 module.exports = adminRouter;
--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,27 +1,36 @@
+/* eslint-disable no-console */
 import express from 'express';
+import {MongoClient as mongodb,} from 'mongodb';
+import {ObjectID as objectId,} from 'mongodb';
 
 const todoRouter = express.Router();
 
-let todos = [
-  {
-    title: 'learn how to manage time',
-    author: 'ignac',
-  },
-  {
-    title: 'CRACKING THE CODING INTERVIEW',
-    author: 'aga',
-  },
-];
-
 todoRouter.route('/')
   .get((req, res) => {
-    res.render('todos', {title: 'todos', todos: todos,});
+    const url = 'mongodb://127.0.0.1:27017/';
+
+    mongodb.connect(url, (err, db) => {
+      if (err) console.log(err);
+      const dbo = db.db('todoapp');
+      dbo.collection('todos').find().toArray((err, results) => {
+        res.render('todos', {title: 'todos', todos: results,});
+      });
+    });
   });
 
 todoRouter.route('/:id')
   .get((req, res) => {
-    let id = req.params.id;
-    res.render('todo',{title: todos[id].title, todo: todos[id],});
+    const id = new objectId(req.params.id);
+    const url = 'mongodb://127.0.0.1:27017/';
+
+
+    mongodb.connect(url, (err, db) => {
+      if (err) console.log(err);
+      const dbo = db.db('todoapp');
+      dbo.collection('todos').findOne({_id: id,}, (err, results) => {
+        res.render('todo', {title: 'single todo', todo: results,});
+      });
+    });
   });
 
 module.exports = todoRouter;
