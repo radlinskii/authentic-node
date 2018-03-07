9026c075ae88b69ad7519e92c1621a0190aeb4c0
radlinskii <radlinsk@student.agh.edu.pl>
2018-03-08 00:50:19 +0100

    :art: hiding config vars to heroku app settings

--- src/strategies/github.strategy.js
+++ src/strategies/github.strategy.js
@@ -4,9 +4,9 @@ import User from '../models/user.model';
 
 module.exports = (passport) => {
   passport.use(new GithubStrategy({
-    clientID: '8dfd0a616bbb2d952ac3',
-    clientSecret: '4d73266279806735692b28856e367610f536c3ee',
-    callbackURL: 'https://authentic-node.herokuapp.com/auth/github/callback', //'http://localhost:8080/auth/github/callback', //
+    clientID: process.env.githubClientID,
+    clientSecret: process.env.githubClientSecret,
+    callbackURL: process.env.githubCalbbackURL,
     passReqToCallback : true,
   },
   function (req, accessToken, refreshToken, profile, done) {
