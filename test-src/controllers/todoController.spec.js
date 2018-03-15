bde65d0a9d7d44c9af9710e6f6468c53b4fc472f
radlinskii <radlinsk@student.agh.edu.pl>
2018-03-15 16:48:43 +0100

    :white_check_mark: added todo middleware test

--- test-src/controllers/todoController.spec.js
+++ test-src/controllers/todoController.spec.js
@@ -1,12 +1,49 @@
-import assert from 'assert';
-const todoController = require('../../lib/controllers/todoController');
+import sinon from 'sinon';
+import todoController from '../../lib/controllers/todoController';
+import { should, } from 'chai';
+should();
+
 describe('TodoController', function () {
   describe('addZeros', function () {
     it('should add zero to number lower than 10', function () {
-      assert.equal('01', todoController.addZeros('1'));
+      todoController.addZeros('1').should.equal('01');
     });
     it('should not add zero to number greater than 10', function () {
-      assert.notEqual('011', todoController.addZeros('11'));
+      todoController.addZeros('11').should.equal('11');
+    });
+  });
+  describe('Middleware', function () {
+    let req;
+    let res;
+    let next;
+    beforeEach(function () {
+      req = {
+        flash: sinon.spy(),
+        isAuthenticated: function () {},
+      };
+      res = {
+        redirect: function () {},
+      };
+      next = sinon.spy();
+    });
+    it('should call next if authenticated', function () {
+      const isAuthenticated = sinon.stub(req, 'isAuthenticated').returns('true');
+
+      todoController.middleware(req, res, next);
+
+      next.calledOnce.should.be.true;
+      isAuthenticated.calledOnce.should.be.true;
+    });
+    it('should redirect to home if not authenticated', function () {
+      const isAuthenticated = sinon.stub(req, 'isAuthenticated').returns(false);
+      const mock = sinon.mock(res);
+
+      mock.expects('redirect').once().withExactArgs('/');
+
+      todoController.middleware(req, res, next);
+
+      isAuthenticated.calledOnce.should.be.true;
+      mock.verify();
     });
   });
 });
