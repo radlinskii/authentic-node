197b79964f4aa5d24e241736eba863c4f6a48d03
radlinskii <radlinsk@student.agh.edu.pl>
2018-03-15 20:57:59 +0100

    :white_check_mark: simple controller tests

--- test-src/controllers/authController.spec.js
+++ test-src/controllers/authController.spec.js
@@ -1,17 +1,9 @@
 import sinon from 'sinon';
-import todoController from '../../lib/controllers/todoController';
+import authController from '../../lib/controllers/authController';
 import { should, } from 'chai';
 should();
 
-describe('TodoController', function () {
-  describe('addZeros', function () {
-    it('should add zero to number lower than 10', function () {
-      todoController.addZeros('1').should.equal('01');
-    });
-    it('should not add zero to number greater than 10', function () {
-      todoController.addZeros('11').should.equal('11');
-    });
-  });
+describe('authController', function () {
   describe('Middleware', function () {
     let req;
     let res;
@@ -28,7 +20,7 @@ describe('TodoController', function () {
     });
     it('should call next if authenticated', function () {
       const isAuthenticated = sinon.stub(req, 'isAuthenticated').returns('true');
-      todoController.middleware(req, res, next);
+      authController.middleware(req, res, next);
       next.calledOnce.should.be.true;
       isAuthenticated.calledOnce.should.be.true;
     });
@@ -36,7 +28,7 @@ describe('TodoController', function () {
       const isAuthenticated = sinon.stub(req, 'isAuthenticated').returns(false);
       const mock = sinon.mock(res);
       mock.expects('redirect').once().withExactArgs('/');
-      todoController.middleware(req, res, next);
+      authController.middleware(req, res, next);
       isAuthenticated.calledOnce.should.be.true;
       mock.verify();
     });
